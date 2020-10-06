350357d5b45639ce4d5aa56a63666d3a4c905804
Nick Pachulski <nick@pachulski.me>
2020-10-05 23:43:16 -0400

    wip

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React from "react"
 import { StyleSheet, Text, View } from "react-native"
 import TrackPlayer from "react-native-track-player"
 
+TrackPlayer.registerPlaybackService(() => require("./TrackPlayerService"))
+
 const track = {
   id: "1",
   url:
@@ -10,44 +12,21 @@ const track = {
   artist: "All Ears English",
 }
 
-const trackPlayerService = async function () {
-  TrackPlayer.addEventListener("remote-play", () => TrackPlayer.play())
-  TrackPlayer.addEventListener("remote-pause", () => TrackPlayer.pause())
-  TrackPlayer.addEventListener("remote-stop", () => TrackPlayer.destroy())
-  TrackPlayer.addEventListener(
-    "remote-jump-forward",
-    async ({ interval }: { interval: number }) => {
-      const position = await TrackPlayer.getPosition()
-      await TrackPlayer.seekTo(position + interval)
-    },
-  )
-  TrackPlayer.addEventListener(
-    "remote-jump-backward",
-    async ({ interval }: { interval: number }) => {
-      const position = await TrackPlayer.getPosition()
-      await TrackPlayer.seekTo(position - interval)
-    },
-  )
-}
-
 export default function App() {
   React.useEffect(() => {
     ;(async () => {
-      await TrackPlayer.setupPlayer().then(() => {
-        // The player is ready to be used
-        console.log("player is setup")
-        TrackPlayer.updateOptions({
-          capabilities: [
-            TrackPlayer.CAPABILITY_PLAY,
-            TrackPlayer.CAPABILITY_PAUSE,
-            TrackPlayer.CAPABILITY_STOP,
-            TrackPlayer.CAPABILITY_JUMP_FORWARD,
-            TrackPlayer.CAPABILITY_JUMP_BACKWARD,
-          ],
-          jumpInterval: 30,
-        })
+      await TrackPlayer.destroy()
+      await TrackPlayer.setupPlayer()
+      await TrackPlayer.updateOptions({
+        capabilities: [
+          TrackPlayer.CAPABILITY_PLAY,
+          TrackPlayer.CAPABILITY_PAUSE,
+          TrackPlayer.CAPABILITY_STOP,
+          TrackPlayer.CAPABILITY_JUMP_FORWARD,
+          TrackPlayer.CAPABILITY_JUMP_BACKWARD,
+        ],
+        jumpInterval: 30,
       })
-      TrackPlayer.registerPlaybackService(() => trackPlayerService)
       await TrackPlayer.add([track])
       await TrackPlayer.play()
     })()
